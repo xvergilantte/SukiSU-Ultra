68f2f5a0ae9937f61dcf1f7d61baa7275bdd2d2e
ShirkNeko <109797057+ShirkNeko@users.noreply.github.com>
2025-08-14 22:19:07 +0800

    kernel: Introducing Tracepoint Hook Type Support Tracepoint is a predefined hook point in the kernel, compared to Kprobe, it is more stable and has lower performance overhead, although compatibility is relatively poor, it is still worth trying
    By the way, we have also included the config definitions related to hook types
    in Kconfig, to enhance cleanliness
    
    Improve and merge types that do not require hooks
    
    Introducing the hook type prctl
    
    These patches is based on backslashxx/KernelSU#5
    
    Co-authored-by: Cloud_Yun <1770669041@qq.com>
    Co-authored-by: Prslc <prslc113@gmail.com>
    Co-authored-by: ShirkNeko <109797057+ShirkNeko@users.noreply.github.com>
    
    Signed-off-by: ShirkNeko <109797057+ShirkNeko@users.noreply.github.com>

--- kernel/ksu.c
+++ kernel/ksu.c
@@ -54,6 +54,10 @@ extern void ksu_sucompat_init();
 extern void ksu_sucompat_exit();
 extern void ksu_ksud_init();
 extern void ksu_ksud_exit();
+#ifdef CONFIG_KSU_TRACEPOINT_HOOK
+extern void ksu_trace_register();
+extern void ksu_trace_unregister();
+#endif
 
 int __init kernelsu_init(void)
 {
@@ -93,6 +97,10 @@ int __init kernelsu_init(void)
 	pr_debug("init ksu driver\n");
 #endif
 
+#ifdef CONFIG_KSU_TRACEPOINT_HOOK
+    ksu_trace_register();
+#endif
+
 #ifdef MODULE
 #ifndef CONFIG_KSU_DEBUG
 	kobject_del(&THIS_MODULE->mkobj.kobj);
@@ -117,6 +125,11 @@ void kernelsu_exit(void)
 #ifdef CONFIG_KSU_KPROBES_HOOK
 	ksu_ksud_exit();
 #endif
+
+#ifdef CONFIG_KSU_TRACEPOINT_HOOK
+    ksu_trace_unregister();
+#endif
+
 	ksu_sucompat_exit();
 
 	ksu_core_exit();
