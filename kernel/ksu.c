bcb38274f925be7708f47f95a0e7ecd889c44f6a
unknow-tech <unknow88886666@gmail.com>
2025-10-08 14:34:59 +0800

    Add support for 6.13 (#454)
    Fiz issue https://github.com/SukiSU-Ultra/SukiSU-Ultra/issues/364
    
    https://github.com/torvalds/linux/commit/cdd30ebb1b9f36159d66f088b61aee264e649d7a

--- kernel/ksu.c
+++ kernel/ksu.c
@@ -3,6 +3,7 @@
 #include <linux/kobject.h>
 #include <linux/module.h>
 #include <linux/workqueue.h>
+#include <linux/version.h>
 
 #include "allowlist.h"
 #include "arch.h"
@@ -154,7 +155,10 @@ MODULE_LICENSE("GPL");
 MODULE_AUTHOR("weishu");
 MODULE_DESCRIPTION("Android KernelSU");
 
-#include <linux/version.h>
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 0, 0)
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 13, 0)
+MODULE_IMPORT_NS("VFS_internal_I_am_really_a_filesystem_and_am_NOT_a_driver");
+#else
 MODULE_IMPORT_NS(VFS_internal_I_am_really_a_filesystem_and_am_NOT_a_driver);
+#endif
 #endif
